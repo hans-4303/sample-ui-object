16b096d6d261d190f6db1529e4c21e49b06e8315
hans <shinezl39@gmail.com>
2024-03-04 00:17:33 +0900

    주석 정리
    유니티 오브젝트 구조가 제한 받겠지만 잘 작동 시킬 수 있는 코드

--- Assets/Custom Assets/Scripts/PlayerSelect.cs
+++ Assets/Custom Assets/Scripts/PlayerSelect.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
+/// <summary>
+/// <para>오브젝트 선택과 필터를 위해 클래스 만들고 활용할 수 있어야 함</para>
+/// </summary>
 public class SelectableShip
 {
+    /// <summary>
+    /// <para>각 배 오브젝트와 배가 포함하는 메시 렌더러, 캔버스로 구분됨</para>
+    /// </summary>
     private GameObject eachShip;
     private List<MeshRenderer> currentMeshRenderers;
     private Canvas eachCanvas;
 
+    /// <summary>
+    /// <para>Prop으로 빼주기</para>
+    /// </summary>
     public GameObject EachShip
     {
         get { return eachShip; }
@@ -28,6 +37,16 @@ public class SelectableShip
         set { eachCanvas = value; }
     }
 
+    /// <summary>
+    /// <para>생성자 작성, 핵심 배 오브젝트 받고 자식 오브젝트 및 컴포넌트 받아오기</para>
+    /// <para>
+    ///     !! GetComponentInChildren || GetComponentInChildren 메서드에 인수로 true, false 넘길 수 있음.
+    ///     false 혹은 작성 안할 시 비활성화된 오브젝트의 컴포넌트는 무시함.
+    ///     true 작성 시 비활성화된 오브젝트의 컴포넌트도 취급함.
+    ///     그래서 GameObject.Find("오브젝트 이름 1").transform.Find("오브젝트 이름 2").gameObject; 같이 접근할 필요 없어짐
+    /// </para>
+    /// </summary>
+    /// <param name="currentShip">배 오브젝트 -> 자식 오브젝트 및 컴포넌트 조회 가능</param>
     public SelectableShip (GameObject currentShip)
     {
         eachShip = currentShip;
@@ -36,6 +55,16 @@ public class SelectableShip
     }
 }
 
+/// <summary>
+/// <para>
+///     아래 같이 선택 및 해제, 상호작용을 분명하게 하는 게 좋다 생각하며
+///     오브젝트 구조 바꾸고 레이어 혹은 태그로 구분해야 함.
+/// </para>
+/// <para>
+///     배(박스 컬라이더 및 레이어 혹은 태그 포함) > 메시 그룹 | 월드 스페이스 캔버스(비활성)로 짜야
+///     Raycast 타겟 인지 > 자식 컴포넌트 접근 등 과정 잘 됨
+/// </para>
+/// </summary>
 public class PlayerSelect : MonoBehaviour
 {
     /// <summary>
@@ -53,6 +82,9 @@ public class PlayerSelect : MonoBehaviour
     /// </summary>
     private RaycastHit hit;
 
+    /// <summary>
+    /// <para>선택할 수 있는 배들 리스트</para>
+    /// </summary>
     private readonly List<SelectableShip> selectableShips = new();
 
     /// <summary>
@@ -79,14 +111,14 @@ public class PlayerSelect : MonoBehaviour
     ///     2. 광선, 대상 오브젝트, 사거리, 목표 레이어 만족할 때와 그렇지 않을 때로 구분
     /// </para>
     /// <para>
-    ///     3. 목표 만족하고 tempShip 오브젝트가 없다면 대상 오브젝트의 컬라이더 - 게임 오브젝트에 접근,
-    ///     렌더러는 tempShip 오브젝트의 자식 Renderer 컴포넌트 모음으로 둠,
-    ///     Renderer 컴포넌트 모음을 순회하면서 materials에 기존 렌더러 및 셰이더 추가.
+    ///     3. 목표 만족하고 선택된 배 오브젝트가 기존 리스트에 있다면 선택 해제 메서드 호출
     /// </para>
     /// <para>
-    ///     4. 목표 만족하지 않으며 tempShip 오브젝트가 있다면
-    ///     렌더러는 tempShip 오브젝트의 자식 Renderer 컴포넌트 모음으로 둠,
-    ///     Renderer 컴포넌트 모음을 순회하면서 materials에 기존 렌더러 보존하고 셰이더 제거
+    ///     4. 목표 만족하고 선택된 배 오브젝트가 기존 리스트에 없다면 새 인스턴스 만들어서 메서드 호출 및 리스트 추가
+    /// </para>
+    /// <para>
+    ///     5. 목표 만족하지 않고 리스트에 추가된 게 있다면 일괄 해제로 인지,
+    ///     List 요소를 제거하기 때문에 역 for문으로 제어함
     /// </para>
     /// </summary>
     private void SelectObject ()
@@ -96,16 +128,19 @@ public class PlayerSelect : MonoBehaviour
         {
             GameObject hitObject = hit.collider.gameObject;
 
+            // FirstOrDefault : 조건에 맞는 첫 요소 혹은 기본 값 반환, 인스턴스니까 기본 값이 Null일 수 있음
             var selectedShip = selectableShips.FirstOrDefault(el => el.EachShip == hitObject);
 
             if (selectedShip != null)
             {
-                Debug.Log("선택 해제");
+                // Debug.Log("선택 해제");
+
                 DeselectObject(selectedShip);
             }
             else
             {
-                Debug.Log("선택");
+                // Debug.Log("선택");
+
                 SelectableShip newSelected = new(hitObject);
                 ApplyShaderAndEnableUI(newSelected);
                 selectableShips.Add(newSelected);
@@ -119,10 +154,25 @@ public class PlayerSelect : MonoBehaviour
                 {
                     DeselectObject(selectableShips[i]);
                 }
+                // Debug.Log("일괄 해제");
             }
         }
     }
 
+    /// <summary>
+    /// <para>셰이더 적용 및 UI 활성 메서드</para>
+    /// <para>
+    ///     1. 선택된 배 메시 렌더러들 Prop에 접근해서 순회, 스코프 내에서 Material List 다루며 Start에서 지정한 shader를 붙임.
+    /// </para>
+    /// <para>
+    ///     2. 1 과정에서의 List를 ToArray();로 대입함.
+    ///     선택된 배 > 각 메시 렌더러 > materials에 접근하기 때문에 배 단독으로 필드나 멤버를 가지지 않아도 됐음.
+    /// </para>
+    /// <para>
+    ///     3. 만약 선택한 배에 캔버스가 있다면 EachCanvas Prop을 통해 활성화 시켜줄 수 있음.
+    /// </para>
+    /// </summary>
+    /// <param name="selected">선택할 수 있는 배를 받음</param>
     private void ApplyShaderAndEnableUI (SelectableShip selected)
     {
         foreach (MeshRenderer meshRenderer in selected.CurrentMeshRenderers)
@@ -136,6 +186,24 @@ public class PlayerSelect : MonoBehaviour
         if (selected.EachCanvas) selected.EachCanvas.gameObject.SetActive(true);
     }
 
+    /// <summary>
+    /// <para>셰이더 해제 및 UI 비활성 메서드</para>
+    /// <para>
+    ///     1. 선택된 배 메시 렌더러들 Prop에 접근해서 순회, 스코프 내에서 Material List 다루며 Start에서 지정한 shader를 제거.
+    /// </para>
+    /// <para>
+    ///     2. 1 과정에서의 List를 ToArray();로 대입함.
+    ///     선택된 배 > 각 메시 렌더러 > materials에 접근하기 때문에 배 단독으로 필드나 멤버를 가지지 않아도 됐음.
+    /// </para>
+    /// <para>
+    ///     3. 만약 선택한 배에 캔버스가 있다면 EachCanvas Prop을 통해 비활성화 시켜줄 수 있음.
+    /// </para>
+    /// <para>
+    ///     4. List에서 선택된 요소를 제거함,
+    ///     선택 및 해제를 통한 단일 적용 및 다른 영역 선택을 통한 일괄 해제 모두 가능함.
+    /// </para>
+    /// </summary>
+    /// <param name="selected"></param>
     private void DeselectObject (SelectableShip selected)
     {
         foreach (MeshRenderer meshRenderer in selected.CurrentMeshRenderers)
